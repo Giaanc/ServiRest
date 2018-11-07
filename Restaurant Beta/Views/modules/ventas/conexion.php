4479881b3c572ff46be3b6884a483f0af81d3a14
Giaanc <44534019+Giaanc@users.noreply.github.com>
2018-11-07 02:40:58 -0300

    cambios
    changes

--- Restaurant Beta/Views/modules/ventas/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php 
- $conexion = new PDO('mysql:host=localhost;dbname=restaurant','root','');
-   date_default_timezone_set('America/Argentina/Buenos_Aires');
- $conexion->exec('SET CHARACTER SET utf8');
-
- ?>
